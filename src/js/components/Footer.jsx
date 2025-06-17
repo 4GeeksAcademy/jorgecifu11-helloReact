bbb077afc902466d3eaa461b983677d330077d05
Jorge Cifuentes Pérez <jorge.cifuentes.perez@gmail.com>
2025-06-17 15:17:01 +0000

    First commit

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <div className="bg-black text-white fixed-bottom">
+            <p>Copyright @ Jorge Cifuentes 2025</p>
+        </div>
+    )
+}
+
+export default Footer;
